36104102d217c66da8bc117c9aed671c1128b145
NameOfFool <ivan.pauk2008@gmail.com>
2023-01-17 20:47:53 +0300

    test

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,6 +3,7 @@
     <head>
         <meta charset="utf-8">
         @vite(['resources/js/app.js'])
+        @vite(['resources/sass/app.scss'])
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>Добро пожаловать</title>
         <!-- Fonts -->
@@ -16,8 +17,22 @@
     </head>
     <body>
 <header>
-    <nav class="navbar">
-        <a href="" class="nav-link">О главном</a>
+    <nav class="navbar navbar-light bg-light">
+        <div class="container-fluid">
+            <a class="navbar-brand">Пресс-центр налоговой грамотности</a>
+            <form class="d-flex input-group w-auto">
+                <input
+                    type="search"
+                    class="form-control rounded"
+                    placeholder="Search"
+                    aria-label="Search"
+                    aria-describedby="search-addon"
+                />
+                <span class="input-group-text border-0" id="search-addon">
+        <i class="fas fa-search"></i>
+      </span>
+            </form>
+        </div>
     </nav>
 </header>
     </body>
