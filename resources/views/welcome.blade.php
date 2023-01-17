e3d944577641df48ba1322b1d2b6db50fb84f0a4
NameOfFool <ivan.pauk2008@gmail.com>
2023-01-17 15:16:41 +0300

    bootstrap connected

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        @vite(['resources/js/app.js'])
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Добро пожаловать</title>
+        <!-- Fonts -->
+        <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+        <!-- Styles -->
+        <style>
+            body {
+                font-family: 'Nunito', sans-serif;
+            }
+        </style>
+    </head>
+    <body>
+<header>
+    <nav class="navbar">
+        <a href="" class="nav-link">О главном</a>
+    </nav>
+</header>
+    </body>
+</html>
