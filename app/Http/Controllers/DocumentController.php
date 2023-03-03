c1a0032d55bb9277f527cdae28696993b0a2788b
NameOfFool <ivan.pauk2008@gmail.com>
2023-03-03 17:15:54 +0300

    stop

--- app/Http/Controllers/DocumentController.php
+++ app/Http/Controllers/DocumentController.php
@@ -4,34 +4,30 @@ namespace App\Http\Controllers;
 
 use App\Models\Document;
 use Illuminate\Http\Request;
-use PhpOffice\PhpWord\Writer\PDF;
-use setasign\Fpdi\Fpdi;
-use setasign\Fpdi\PdfParser\PdfParser;
-use Smalot\PdfParser\Parser;
+use mikehaertl\pdftk\Command;
+use \mikehaertl\pdftk\Pdf;
 
 class DocumentController extends Controller
 {
     public function index()
     {
         $documents = Document::all();;
-        $path = public_path("Fool.pdf");
-
-        $output = public_path("storage/foolishDoc.pdf");
+        $path = public_path("Form.pdf");
         $inn = "7  7  2  6  4  9  2  0  4  8  8  6";
+        $pdf = new Pdf($path, [
+            'command' => 'C:\Program Files (x86)\PDFtk\bin\pdftk.exe',
+            'useExec' => true,
+            'locale' => 'en_US.utf8',
+            'procEnv' => [
+                'LANG' => 'en_US.utf-8',
+            ],
+        ]);
 
-        $fpdi = new Fpdi;
-        $fpdi->SetFont("helvetica","",13);
-        $parser = new Parser();
-        $pdf = $parser->parseFile($path);
-        $text = $pdf->getPages()[0]->getDataTm();
-        $count = $fpdi->setSourceFile($path);
-        $template = $fpdi->importPage(1);
-        $size = $fpdi->getTemplateSize($template);
-        $fpdi->AddPage($size['orientation'],array($size['width'],$size['height']));
-        $fpdi->useTemplate($template);
-        $fpdi->Text(66,10,$inn);
-        $fpdi->Output($output,"F");
-        dd($text);
-
+        $result = $pdf->fillForm([
+           'Text1' =>$inn
+        ])->needAppearances()->saveAs("foolish_form.pdf");
+        if ($result === false) {
+            dd($pdf->getError());
+        }
     }
 }
