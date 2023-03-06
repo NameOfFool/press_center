b820a01b8de8056f4fff755abbc26e0f3214d160
NameOfFool <ivan.pauk2008@gmail.com>
2023-03-06 15:56:40 +0300

    update pdf filling

--- app/Http/Controllers/DocumentController.php
+++ app/Http/Controllers/DocumentController.php
@@ -12,19 +12,19 @@ class DocumentController extends Controller
     public function index()
     {
         $documents = Document::all();;
+    }
+
+    public function fillForm()
+    {
         $path = public_path("Form.pdf");
-        $inn = "7  7  2  6  4  9  2  0  4  8  8  6";
+
+        $inn = "772649204886";
         $pdf = new Pdf($path, [
             'command' => 'C:\Program Files (x86)\PDFtk\bin\pdftk.exe',
             'useExec' => true,
-            'locale' => 'en_US.utf8',
-            'procEnv' => [
-                'LANG' => 'en_US.utf-8',
-            ],
         ]);
-
         $result = $pdf->fillForm([
-           'Text1' =>$inn
+            'Text1' => $inn
         ])->needAppearances()->saveAs("foolish_form.pdf");
         if ($result === false) {
             dd($pdf->getError());
